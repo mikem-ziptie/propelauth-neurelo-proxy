1166cb20da514905272f6265e89144dcfc1a9dbc
Mike McCune <mike@ziptie.app>
2024-04-08 10:21:13 -0700

    Actually return a 401 if the validation fails + debug logs

--- api_proxy.py
+++ api_proxy.py
@@ -1,29 +1,33 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
+import uvicorn
 import httpx
 import os
 from propelauth_py import init_base_auth
 from propelauth_py import UnauthorizedException
+
 
 class APIProxy:
     def __init__(self):
         self.auth = init_base_auth(
             os.getenv("PROPEL_AUTH_URL"),
             os.getenv("PROPEL_AUTH_API_KEY"),
-        )          
+        )
+        print("os.getenv(PROPEL_AUTH_URL)", os.getenv("PROPEL_AUTH_URL"))
+        print("os.getenv(PROPEL_AUTH_API_KEY)", os.getenv("PROPEL_AUTH_API_KEY"))
         self.forward_domain = os.getenv("NEURELO_URL")
-        self.additional_header_name = 'X-API-KEY'
-        self.additional_header_value = os.getenv('NEURELO_API_KEY')
+        self.additional_header_name = "X-API-KEY"
+        self.additional_header_value = os.getenv("NEURELO_API_KEY")
 
     async def forward_request(self, request: Request, path: str):
-        
-        auth_header = request.headers.get('Authorization')
+
+        auth_header = request.headers.get("Authorization")
         try:
             user = self.auth.validate_access_token_and_get_user(auth_header)
             print("Logged in as", user.user_id)
         except UnauthorizedException:
             print("Invalid access token")
+            raise HTTPException(status_code=401, detail="Invalid access token")
 
-        
         forward_url = f"{self.forward_domain}/{path}"
         headers = {key: value for key, value in request.headers.items()}
         headers[self.additional_header_name] = self.additional_header_value
@@ -34,13 +38,15 @@ class APIProxy:
                 url=forward_url,
                 headers=headers,
                 content=await request.body(),
-                params=request.query_params
+                params=request.query_params,
             )
 
         return response.text, response.status_code, response.headers.items()
 
+
 app = FastAPI()
 api_proxy = APIProxy()
+
 
 @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def forward_request(request: Request, path: str):
@@ -48,5 +54,4 @@ async def forward_request(request: Request, path: str):
 
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
